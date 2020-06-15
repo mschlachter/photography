0fcc537cf8307360cbd9dc7d9cb0efd38925ddd3
Matthew Schlachter <matthew@webmann.info>
2020-06-15 17:27:54 -0400

    Change page title on dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ __('Material Dashboard Laravel - Free Frontend Preset for Laravel') }}</title>
+    <title>{{ $titlePage }} — Photography | Matthew Schlachter Admin</title>
     <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('material') }}/img/apple-icon.png">
     <link rel="icon" type="image/png" href="{{ asset('material') }}/img/favicon.png">
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
@@ -60,7 +60,7 @@
               </li>
               <li>
                 <a class="img-holder switch-trigger" href="javascript:void(0)">
-                  <img src="{{ asset('material') }}/img/sidebar-3.jpg" alt="">
+                  <img src="{{ asset('material') }}/img/sidebar-3.jpg" alt=""> __('Material Dashboard Laravel - Free Frontend Preset for Laravel') 
                 </a>
               </li>
               <li>
