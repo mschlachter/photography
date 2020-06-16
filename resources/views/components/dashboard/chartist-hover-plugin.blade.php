1636bc02423d2c2d39ed5a7dcc9ab7dc7ff87d54
Matthew Schlachter <matthew@webmann.info>
2020-06-16 07:48:59 -0400

    Refactor: clean up centralized chartist hover plugin reference

--- resources/views/components/dashboard/chartist-hover-plugin.blade.php
+++ resources/views/components/dashboard/chartist-hover-plugin.blade.php
@@ -1,6 +1,8 @@
-@push('js')
-  @if(!config('graph-assets-loaded', false) && !config()->set('graph-assets-loaded', true))
-    <script src="https://cdn.jsdelivr.net/npm/chartist-plugin-tooltips@0.0.17/dist/chartist-plugin-tooltip.min.js" integrity="sha256-BdDMib6f/EOwrxY3YE9bfqySmqixP5zvookyxS1khtY=" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/chartist-plugin-tooltips@0.0.17/dist/chartist-plugin-tooltip.min.css">
-  @endif
-@endpush
+@if(!config('graph-assets-loaded', false) && !config()->set('graph-assets-loaded', true))
+	@push('css')
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/chartist-plugin-tooltips@0.0.17/dist/chartist-plugin-tooltip.min.css" defer>
+	@endpush
+	@push('js')
+		<script src="https://cdn.jsdelivr.net/npm/chartist-plugin-tooltips@0.0.17/dist/chartist-plugin-tooltip.min.js" integrity="sha256-BdDMib6f/EOwrxY3YE9bfqySmqixP5zvookyxS1khtY=" crossorigin="anonymous"></script>
+	@endpush
+@endif
