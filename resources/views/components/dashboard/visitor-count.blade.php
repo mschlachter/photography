3ec478bf09d1f8837956ffdb9d7dcecdf204852c
Matthew Schlachter <matthew@webmann.info>
2020-06-17 06:19:40 -0400

    Use AJAX for 'Visitor Count' dashboard widget

--- resources/views/components/dashboard/visitor-count.blade.php
+++ resources/views/components/dashboard/visitor-count.blade.php
@@ -1,15 +1,10 @@
-@inject('analytics', App\Libraries\ToolboxGoogleAnalytics)
-@php
-  // Get our values for the dashboard:
-$sessionsForLast7Days = $analytics->getSessionsForLast7Days();
-@endphp
 <div class="card card-stats">
   <div class="card-header card-header-success card-header-icon">
     <div class="card-icon">
       <i class="material-icons">store</i>
     </div>
     <p class="card-category">Visitors</p>
-    <h3 class="card-title">{{ $sessionsForLast7Days }}</h3>
+    <h3 class="card-title" id="visitorCount">Loading…</h3>
   </div>
   <div class="card-footer">
     <div class="stats">
@@ -17,3 +12,16 @@ $sessionsForLast7Days = $analytics->getSessionsForLast7Days();
     </div>
   </div>
 </div>
+
+@push('js')
+<script>
+  $(document).ready(function() {
+    // get our data:
+    $.ajax('{{ route('admin.dashboard.data.visitor-count') }}', {
+      success: function(data) {
+        document.getElementById('visitorCount').innerHTML = data;
+      }
+    });
+  });
+</script>
+@endpush
