c55723a8e7fe9094a70b7f229fd089b2d7a7162f
Matthew Schlachter <matthew@webmann.info>
2020-06-16 21:55:43 -0400

    Use dynamic icon colour for 'get more space' icon

--- resources/views/components/dashboard/free-space.blade.php
+++ resources/views/components/dashboard/free-space.blade.php
@@ -17,7 +17,7 @@ $class = min((int)log($bytes , $base) , count($si_prefix) - 1);
   </div>
   <div class="card-footer">
     <div class="stats">
-      <i class="material-icons text-danger">warning</i>
+      <i class="material-icons text-{{ $bytes < 1e+7 ? 'danger' : 'warning' }}">warning</i>
       <a href=" mailto:sales@schlachter.ca?subject=Request%20for%20more%20space%3A%20{{ config('app.url') }}&body=Hello%2C%0D%0A%0D%0AI%20would%20like%20to%20request%20more%20space%20on%20my%20photography%20website%20because%20I%20only%20have%20{{ sprintf('%1.2f' , $bytes / pow($base,$class)) . ' ' . $si_prefix[$class] }}%20left.%0D%0A%0D%0AI%20would%20like%20to%20request%3A%20%5B%5B%20The%20amount%20of%20additional%20space%20you%20would%20like%20to%20request%20%5D%5D%0D%0A%0D%0AThank%20you%2C%0D%0A%5B%5B%20Your%20name%20%5D%5D">Get More Space...</a>
     </div>
   </div>
