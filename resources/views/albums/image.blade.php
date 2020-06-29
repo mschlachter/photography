865fe12778d636c3c1d2ab63866465d6e8c5287c
Matthew Schlachter <matthew@webmann.info>
2020-06-29 10:31:47 -0400

    Move image meta data to centralized component

--- resources/views/albums/image.blade.php
+++ resources/views/albums/image.blade.php
@@ -28,32 +28,5 @@
     </a>
     @endif
 </div>
-<div class="container container-fluid image-details">
-    <h2>
-        Meta info
-    </h2>
-    <p>
-        Title: {{ $image->title }}
-    </p>
-    <p>
-        Date: {{ $image->date }}
-    </p>
-    <p>
-        Album: 
-        <a href="{{ route('albums.show', ['album' => $image->album]) }}#image-{{ $image->id }}">{{ $image->album->title }}</a>
-    </p>
-    <p>
-        Tags:
-        @foreach($image->tags->pluck('name')->toArray() as $tag)
-            <a href="{{ route('images.all', ['tag[]' => $tag]) }}">{{ $tag }}</a>@if(!$loop->last),@endif
-        @endforeach
-    </p>
-    <p>
-        <a href="{{ route('download', ['image' => $image]) }}" target="_blank">
-            Download full-size version
-        </a>
-    </p>
-    <p><x-share/></p>
-    <x-comments :pageId="'photography/images/' . $image->id"></x-comments>
-</div>
+<x-image-meta :image="$image"/>
 @endsection
