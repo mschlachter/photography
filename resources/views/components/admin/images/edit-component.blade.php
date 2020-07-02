7be9e0e663608a7a884a9a846a8a80154635c4ef
Matthew Schlachter <matthew@webmann.info>
2020-07-01 20:38:37 -0400

    Remove unneeded ending slash from edit image component

--- resources/views/components/admin/images/edit-component.blade.php
+++ resources/views/components/admin/images/edit-component.blade.php
@@ -6,7 +6,7 @@
             @method('PUT')
             <div class="form-group{{ $errors->has('title-' . $image->id) ? ' has-danger' : '' }}">
                 <label for="input-title-{{ $image->id }}">{{ __('Title') }}</label>
-                <input class="form-control{{ $errors->has('title-' . $image->id) ? ' is-invalid' : '' }}" name="title-{{ $image->id }}" id="input-title-{{ $image->id }}" type="text" placeholder="{{ __('Title') }}" value="{{ old('title-' . $image->id, $image->title) }}" required="true" aria-required="true" />
+                <input class="form-control{{ $errors->has('title-' . $image->id) ? ' is-invalid' : '' }}" name="title-{{ $image->id }}" id="input-title-{{ $image->id }}" type="text" placeholder="{{ __('Title') }}" value="{{ old('title-' . $image->id, $image->title) }}" required="true" aria-required="true">
                 @if ($errors->has('title-' . $image->id))
                 <span id="title-error-{{ $image->id }}" class="error text-danger" for="input-title-{{ $image->id }}">{{ $errors->first('title-' . $image->id) }}</span>
                 @endif
@@ -20,14 +20,14 @@
             </div>
             <div class="form-group{{ $errors->has('tags-' . $image->id) ? ' has-danger' : '' }}">
                 <label for="input-tags-{{ $image->id }}">{{ __('Tags') }}</label>
-                <input class="form-control tags-input border-top-0 border-left-0 border-right-0{{ $errors->has('tags-' . $image->id) ? ' is-invalid' : '' }}" name="tags-{{ $image->id }}" id="input-tags-{{ $image->id }}" type="text" placeholder="{{ __('Tags') }}" value="{{ old('tags-' . $image->id, implode(', ', $image->tags->pluck('name')->toArray())) }}" />
+                <input class="form-control tags-input border-top-0 border-left-0 border-right-0{{ $errors->has('tags-' . $image->id) ? ' is-invalid' : '' }}" name="tags-{{ $image->id }}" id="input-tags-{{ $image->id }}" type="text" placeholder="{{ __('Tags') }}" value="{{ old('tags-' . $image->id, implode(', ', $image->tags->pluck('name')->toArray())) }}">
                 @if ($errors->has('tags-' . $image->id))
                 <span id="tags-error-{{ $image->id }}" class="error text-danger" for="input-tags-{{ $image->id }}">{{ $errors->first('tags-' . $image->id) }}</span>
                 @endif
             </div>
             <div class="form-group{{ $errors->has('date-' . $image->id) ? ' has-danger' : '' }}">
                 <label for="input-date-{{ $image->id }}">{{ __('Date') }}</label>
-                <input class="form-control{{ $errors->has('date-' . $image->id) ? ' is-invalid' : '' }}" name="date-{{ $image->id }}" id="input-date-{{ $image->id }}" type="date" placeholder="{{ __('Date') }}" value="{{ old('date-' . $image->id, $image->date) }}" required="true" aria-required="true" />
+                <input class="form-control{{ $errors->has('date-' . $image->id) ? ' is-invalid' : '' }}" name="date-{{ $image->id }}" id="input-date-{{ $image->id }}" type="date" placeholder="{{ __('Date') }}" value="{{ old('date-' . $image->id, $image->date) }}" required="true" aria-required="true">
                 @if ($errors->has('date-' . $image->id))
                 <span id="date-error-{{ $image->id }}" class="error text-danger" for="input-date-{{ $image->id }}">{{ $errors->first('date-' . $image->id) }}</span>
                 @endif
