40a76de9217067084eeb16601d760485c12982c3
Matthew Schlachter <matthew@webmann.info>
2020-07-01 22:57:40 -0400

    Cache image dimensions for dashboard use

--- resources/views/components/picture.blade.php
+++ resources/views/components/picture.blade.php
@@ -4,9 +4,14 @@ return;
 }
 $media = $image->getFirstMedia('image');
 $webp = $media->hasGeneratedConversion('webp') && $media->getGeneratedConversions()['webp'];
-$spatieImage = Spatie\Image\Image::load($image->getFirstMediaPath('image'));
-$width = $spatieImage->getWidth();
-$height = $spatieImage->getHeight();
+$dimensions = Cache::remember('image|dimensions|' . $image->id, 60*60*24*30*12, function() use ($image) {
+    $spatieImage = Spatie\Image\Image::load($image->getFirstMediaPath('image'));
+    $width = $spatieImage->getWidth();
+    $height = $spatieImage->getHeight();
+    return compact('width','height');
+});
+$width = $dimensions ? $dimensions['width'] : 1;
+$height = $dimensions ? $dimensions['height'] : 1;
 @endphp
 <picture>
     @if($webp !== false)
